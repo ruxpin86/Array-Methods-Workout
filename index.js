fa450f1ce44aa9f7e5425f5a6fc9ed02102783d7
Ted G <61029718+ruxpin86@users.noreply.github.com>
2022-09-03 17:25:45 -0600

    solved for 7 and need to refactor to ES6 eventually

--- index.js
+++ index.js
@@ -117,14 +117,26 @@ console.table(oldest);
 // 6. create a list of Boulevards in Paris that contain 'de' anywhere in the name
 // https://en.wikipedia.org/wiki/Category:Boulevards_in_Paris
 
-const category = document.querySelector(".mw-category");
-const links = Array.from(category.querySelectorAll("a"));
-const de = links
-  .map((link) => link.textContent)
-  .filter((streetName) => streetName.includes("de"));
+// const category = document.querySelector(".mw-category");
+// const links = Array.from(category.querySelectorAll("a"));
+// const de = links
+//   .map((link) => link.textContent)
+//   .filter((streetName) => streetName.includes("de"));
 
 // 7. sort Exercise
 // Sort the people alphabetically by last name
+
+const alphabetically = people.sort(function (lastName, nextName) {
+  const [aLast, aFirst] = lastName.split(", ");
+  const [bLast, bFirst] = nextName.split(", ");
+  if (aLast > bLast) {
+    return 1;
+  } else {
+    return -1;
+  }
+});
+
+console.log(alphabetically);
 
 // 8. Reduce Exercise
 // Sum up the instances of each of these
